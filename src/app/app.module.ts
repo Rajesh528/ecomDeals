5b8d7e11cca2af5866b2e3f158f4f9ca6345384e
Rajesh528 <rajeshkumar.mithinti@gmail.com>
2025-05-26 17:25:44 +0530

    auth updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { MetaReducer, StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -19,6 +19,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 // import { ProductEffects } from './store/effects/product.effects';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { usersReducer } from './store/reducers/user.reducer';
+
+export function localStorageSyncReducer(reducer: any): any {
+  return localStorageSync({
+    keys: ['users'], // only persist auth state
+    rehydrate: true
+  })(reducer);
+}
+
+const metaReducers: MetaReducer[] = [localStorageSyncReducer];
 
 @NgModule({
   declarations: [
@@ -32,13 +43,15 @@ import { environment } from '../environments/environment';
 
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule, 
     AppRoutingModule,
     ReactiveFormsModule,
     CommonModule,
     HttpClientModule,
     EffectsModule.forRoot([ProductEffects,AuthEffects]),
-    StoreModule.forRoot({ auth: authReducer,productState: productReducer }),
+    StoreModule.forRoot({ auth: authReducer,productState: productReducer , users: usersReducer},
+      { metaReducers }
+    ),
     EffectsModule.forFeature([AuthEffects]),
     StoreDevtoolsModule.instrument({
     maxAge: 25, // Retains last 25 states
